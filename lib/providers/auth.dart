d499c2d0d7ef8f95bf89870bb30dbfcd85d0b72e
Yusufdevv <yusufnurullayev03@gmail.com>
2022-07-14 09:04:31 +0500

    added logout() function

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -15,8 +15,8 @@ class Auth with ChangeNotifier {
     return _token != null;
   }
 
-  String get userId {
-    return _userId!;
+  String? get userId {
+    return _userId;
   }
 
   String? get token {
@@ -71,4 +71,11 @@ class Auth with ChangeNotifier {
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
+
+  void logout() {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    notifyListeners();
+    }
 }
--- lib/widgets/appdrawer.dart
+++ lib/widgets/appdrawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:markett_app/providers/auth.dart';
 import 'package:markett_app/screens/home_screen.dart';
 import 'package:markett_app/screens/manage_products_screen.dart';
 import 'package:markett_app/screens/orders_screen.dart';
+import 'package:provider/provider.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
@@ -34,6 +36,15 @@ class AppDrawer extends StatelessWidget {
                 .pushReplacementNamed(ManageProductsScreen.routeName),
             leading:const Icon(Icons.settings),
             title:const Text("Mahsulotlarni boshqarish"),
+          ),
+          const Divider(),
+          ListTile(
+            onTap: () {
+              Navigator.of(context).pop();
+              Provider.of<Auth>(context, listen: false).logout();
+            } ,
+            leading:const Icon(Icons.exit_to_app),
+            title:const Text("Chiqish"),
           )
         ],
       ),
